9870864cd2292b16990bb98daf6516ce0a76b409
YoonSangYoon <yk19888@naver.com>
2022-12-15 06:13:08 +0900

    리로드 무기 회전

--- Source/FPSGame/Private/Character/PlayerAnimInstance.cpp
+++ Source/FPSGame/Private/Character/PlayerAnimInstance.cpp
@@ -80,4 +80,7 @@ void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaTime)
 	}
 
 	bUseFABRIK = PlayerCharacter->GetCombatState() != ECombatState::ECS_Reloading;
+	bUseAimOffsets = PlayerCharacter->GetCombatState() != ECombatState::ECS_Reloading;
+	bTransformRightHand = PlayerCharacter->GetCombatState() != ECombatState::ECS_Reloading;
+	
 }
