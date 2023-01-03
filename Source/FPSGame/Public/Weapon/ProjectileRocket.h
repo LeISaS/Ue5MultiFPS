11eaf4dc7b61ea354ff8df81d3ba833ed712a0b6
YoonSangYoon <yk19888@naver.com>
2023-01-03 15:59:59 +0900

    Rocket Class & BP

---
+++ Source/FPSGame/Public/Weapon/ProjectileRocket.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Projectile.h"
+#include "ProjectileRocket.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSGAME_API AProjectileRocket : public AProjectile
+{
+	GENERATED_BODY()
+
+public:
+	AProjectileRocket();
+protected:
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpusle, const FHitResult& Hit) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* RocketMesh;
+};
